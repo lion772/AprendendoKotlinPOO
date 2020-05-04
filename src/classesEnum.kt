763a76aa08997ad3e369a9fd0e2c8bbb9c55c9cc
William Lopes <williamlopes64@gmail.com>
2020-05-04 20:35:52 -0300

    modificações na classe Enum

--- src/classesEnum.kt
+++ src/classesEnum.kt
@@ -1,11 +1,32 @@
-enum class prioridade {
+enum class Prioridade1 {
     /* A classe Enum nos permite enumerar, listar possibilidades que a gente pode
     * ter de valores */
 
     BAIXA, MEDIA, ALTA
 }
+//Override
+enum class Prioridade2(var id: Int){
+    BAIXA(1){
+        override fun toString(): String {
+            return "super baixa, não se preocupe"
+        }
+    },
+    MEDIA(5), ALTA(10)
+}
+
+class Alarme(var des:String, p:Prioridade2)
 
 fun main() {
 
-    prioridade.BAIXA
+    //println(Prioridade1.BAIXA)
+    for(p in Prioridade2.values()) {
+
+        if (p.toString() == "MEDIA") println("Essa é verdade")
+
+        println("$p - ${p.id} - ${p.ordinal}")
+    }
+    Alarme("Aviso!", Prioridade2.ALTA)
+
+
+
 }
