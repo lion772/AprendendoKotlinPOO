3ca7d6c68826ab1cbe995c2c3a3e6e5cf84d7c86
William Lopes <williamlopes64@gmail.com>
2020-05-05 01:33:35 -0300

    adicionando descricao

--- src/classesEnum.kt
+++ src/classesEnum.kt
@@ -1,11 +1,12 @@
 enum class Prioridade1 {
     /* A classe Enum nos permite enumerar, listar possibilidades que a gente pode
-    * ter de valores */
+    * ter de valores. É um tipo de classe que tem um conjunto de valores constantes.
+    * Exemplo simples que não deve ser novidade para você, exceto pela palavra class a mais: */
 
     BAIXA, MEDIA, ALTA
 }
 //Override
-enum class Prioridade2(var id: Int){
+enum class Prioridade2(var id: Int){ //Pode ter construtor
     BAIXA(1){
         override fun toString(): String {
             return "super baixa, não se preocupe"
@@ -23,7 +24,7 @@ fun main() {
 
         if (p.toString() == "MEDIA") println("Essa é verdade")
 
-        println("$p - ${p.id} - ${p.ordinal}")
+        println("$p - ${p.id} - ${p.ordinal}") //Pode ter funções e propriedades. Ordinal se refere à posição do atributo dentro da classe
     }
     Alarme("Aviso!", Prioridade2.ALTA)
 
